a8d0a66316453af73460803ec6295bd463f9d8f9
David Fedele <dave.fedele@gmail.com>
2015-04-30 12:19:32 -0700

    no select for heroku

--- app.js
+++ app.js
@@ -11,12 +11,11 @@ var redis = require('redis');
 if( process.env.REDISTOGO_URL ){
   var rtg = require('url').parse(process.env.REDISTOGO_URL);
   var client = redis.createClient(rtg.port, rtg.hostname);
-  client.auth(rtg.auth.splut(":")[1]);
+  client.auth(rtg.auth.split(":")[1]);
 }else{
   var client = redis.createClient();
+  client.select((process.env.NODE_ENV || 'development').length);
 }
-
-client.select((process.env.NODE_ENV || 'development').length);
 // End Redis Connection
 
 // client.hset('todos', 'Laundry', 'wash clothes and sheets too');
